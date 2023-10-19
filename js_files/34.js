43c0d6e5928ec21608ceeae7db4a363da130da0b
Aman Tomar <93078419+AmanTomar02@users.noreply.github.com>
2023-10-19 16:14:21 +0000

    con

--- js_files/34.js
+++ js_files/34.js
@@ -1,2 +1,9 @@
+const ChangeBGRed = () => {
+    document.body.firstElementChild.style.background = "red"
+}
+
+
 console.log("firstChild : ", document.firstChild);
 console.log("firstElementChild : ", document.firstElementChild);
+
+// Note: by this we can style or access the document element and node 
