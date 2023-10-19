f141c901c105e7f4100178bff9cc54607fc168c1
Aman Tomar <93078419+AmanTomar02@users.noreply.github.com>
2023-10-19 16:53:50 +0000

    congi

--- js_files/35.js
+++ js_files/35.js
@@ -1,6 +1,9 @@
-let t = document.body.firstElementChild.firstElementChild
-console.log(t)
-console.log(t.tbody)   
-console.log(t.rows[2].rowIndex)
-console.log(t.caption)
-console.log(t.tHead)
+// let x= document.body.firstElementChild.firstElementChild
+// console.log(x)
+// console.log(t.tbody)
+// console.log(t.rows[2].rowIndex)
+// console.log(t.caption)
+// console.log(t.tHead)
+
+
+
