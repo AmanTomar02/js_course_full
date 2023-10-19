872e622b9a4b5d83c10ff3edcc29f58ba455e15e
Aman Tomar <93078419+AmanTomar02@users.noreply.github.com>
2023-10-19 15:51:48 +0000

    config

--- js_files/34.js
+++ js_files/34.js
@@ -1,3 +1,2 @@
-let b = document.body
-console.log("First child of b is :", b.firstChild)
-console.log("First Element child of b is :", b.firstElementChild)
+console.log("firstChild : ", document.firstChild);
+console.log("firstElementChild : ", document.firstElementChild);
