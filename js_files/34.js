a516690cbcf0db6cdbb47e4597ba7c8607de9d49
Aman Tomar <93078419+AmanTomar02@users.noreply.github.com>
2023-10-19 15:27:05 +0000

    config

--- js_files/34.js
+++ js_files/34.js
@@ -1,7 +1,3 @@
-// const changeBG = () => {
-
-//     document.body.firstElementChild.style.background = "red"
-// }
 let b = document.body
 console.log("First child of b is :", b.firstChild)
-console.log("First_Element_child of b is :", b.firstElementChild)
+console.log("First Element child of b is :", b.firstElementChild)
