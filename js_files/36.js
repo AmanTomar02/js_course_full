bc72420ef8a417e96f782e2f25cc068ce2190a7b
Aman Tomar <93078419+AmanTomar02@users.noreply.github.com>
2023-10-19 17:08:25 +0000

    config

--- js_files/36.js
+++ js_files/36.js
@@ -1,29 +1,29 @@
-// let x = document.getElementById("firstcardtitle")
-// x.style.color = "green"
+let x = document.getElementById("firstcardtitle")
+x.style.color = "green"
 
-// let x1 = document.querySelectorAll(".card-title")
-// // console.log(x1)
-// x1[0].style.color="gree"
-// x1[1].style.color="blue"
-// x1[2].style.color="red"
-// document.querySelector(".")
+let x1 = document.querySelectorAll(".card-title")
+// console.log(x1)
+x1[0].style.color="gree"
+x1[1].style.color="blue"
+x1[2].style.color="red"
+document.querySelector(".")
 
-const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
-const appendAlert = (message, type) => {
-  const wrapper = document.createElement('div')
-  wrapper.innerHTML = [
-    `<div class="alert alert-${type} alert-dismissible" role="alert">`,
-    `   <div>${message}</div>`,
-    '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
-    '</div>'
-  ].join('')
+// const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+// const appendAlert = (message, type) => {
+//   const wrapper = document.createElement('div')
+//   wrapper.innerHTML = [
+//     `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+//     `   <div>${message}</div>`,
+//     '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+//     '</div>'
+//   ].join('')
 
-  alertPlaceholder.append(wrapper)
-}
+//   alertPlaceholder.append(wrapper)
+// }
 
-const alertTrigger = document.getElementById('liveAlertBtn')
-if (alertTrigger) {
-  alertTrigger.addEventListener('click', () => {
-    appendAlert('Nice, you triggered this alert message!', 'success')
-  })
-}
+// const alertTrigger = document.getElementById('liveAlertBtn')
+// if (alertTrigger) {
+//   alertTrigger.addEventListener('click', () => {
+//     appendAlert('Nice, you triggered this alert message!', 'success')
+//   })
+// }
