46e893efd995b60fadb2e7f419e22149dc57df5c
Aman Tomar <93078419+AmanTomar02@users.noreply.github.com>
2023-10-20 08:01:50 +0000

    config

--- js_files/37.js
+++ js_files/37.js
@@ -1,8 +1,25 @@
 let id1 = document.getElementById("id1")
-console.log(id1)
-console.log(id1.matches(".class"))// its false 
-console.log(id1.matches(".box"))  // tis true
-console.log(sp1.closest(".box"))
-console.log(sp1.closest("#sp1"))
-console.log(id1.contains(sp1))
-console.log(sp1.contains(sp1))
+// console.log(id1)
+
+
+/********* .matches ********/ 
+// NOTE: it only check if element matchs the given css selector 
+
+// console.log(id1.matches(".class"))// its false
+// console.log(id1.matches(".box"))  // tis true
+
+
+/********* .closest ********/
+// NOTE: it only check nearest ancestor matches the given css selector 
+
+// console.log(sp1.closest(".box"))
+// console.log(sp1.closest("#sp1"))
+
+
+/********* .contains ********/
+
+// NOTE: it return true if element-A is inside element-B 
+
+console.log(id1.contains(sp1)) //true
+console.log(id2.contains(sp1)) //flase
+console.log(sp1.contains(sp1))  //true
