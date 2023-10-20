41e84a2eb67b1287aa35b211dd68355c641791a1
Aman Tomar <93078419+AmanTomar02@users.noreply.github.com>
2023-10-20 09:47:46 +0000

    config

--- js_files/38.js
+++ js_files/38.js
@@ -5,7 +5,12 @@ document.getElementsByTagName("nav")[0].firstElementChild.style.color = "red"
 
 // prob:3
 
-document.getElementsByTagName("nav")[0].firstElementChild.style.color = "yellow"
-document.getElementsByTagName("nav")[0].lastElementChild.style.color = "yellow"
+document.getElementsByTagName("nav")[0].firstElementChild.style.color = "blue"
+document.getElementsByTagName("nav")[0].lastElementChild.style.color = "blue"
 
-// prble
+// prob-4
+// make all li bg red
+
+Array.from(document.getElementsByTagName("li")).forEach((element) => {
+    element.style.background = "yellow";
+})
