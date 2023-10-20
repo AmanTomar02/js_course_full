7f617bc0a987a188149d581c657a979c1f1142a1
Aman Tomar <93078419+AmanTomar02@users.noreply.github.com>
2023-10-20 09:40:02 +0000

    con\fig

--- js_files/38.js
+++ js_files/38.js
@@ -1,8 +1,11 @@
 // chapter 7 
 
 // prob:1
-// document.getElementsByTagName("nav")[0].firstElementChild.style.color = "red"
 document.getElementsByTagName("nav")[0].firstElementChild.style.color = "red"
-document.getElementsByTagName("nav")[0].lastElementChild.style.color = "red"
 
-Array.from(document.getElementsByTagName("li")).forEach((Element) => { Element.style.background = "yellow" })
+// prob:3
+
+document.getElementsByTagName("nav")[0].firstElementChild.style.color = "yellow"
+document.getElementsByTagName("nav")[0].lastElementChild.style.color = "yellow"
+
+// prble
