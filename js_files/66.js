3a19b63a6e7291e6a93e759a887fd41fec4652b3
Aman Tomar <93078419+AmanTomar02@users.noreply.github.com>
2023-11-07 15:57:33 +0000

    cons

--- js_files/66.js
+++ js_files/66.js
@@ -1,5 +1,8 @@
 let p = fetch("https://goweather.herokuapp.com/weather/india")
 p.then((response) => {
+    console.log(response.headers);
+    console.log(response.status);
+    console.log(response.ok);
     return response.json()
 }).then((response2) => {
     console.log(response2)
