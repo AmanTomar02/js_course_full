fb557d65de786617452a73ed84e4227644837297
Aman Tomar <93078419+AmanTomar02@users.noreply.github.com>
2023-11-07 14:10:01 +0000

    congi

---
+++ js_files/61.js
@@ -0,0 +1,24 @@
+// Error Object & Custom Errors 
+
+// let age = prompt("enter the age ")
+// age = Number.parseInt(age)
+
+// if (age > 50) {
+//     throw new ReferenceError("this is  age 50 salmaan boii ")
+// }
+
+// output : Uncaught ReferenceError: this is  age 50 salmaan boii 
+
+
+try {
+    let a = 0
+    console.log(programm)
+    console.log("successfully");
+
+} catch {
+    console.log(p);
+    console.log("error");
+}
+finally {
+    console.log("good boy")
+}
