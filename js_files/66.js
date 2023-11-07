b543077bcea8e5a36ba9bca5968850867d54cb4a
Aman Tomar <93078419+AmanTomar02@users.noreply.github.com>
2023-11-07 15:53:04 +0000

    cind

--- js_files/66.js
+++ js_files/66.js
@@ -1,10 +1,6 @@
 let p = fetch("https://goweather.herokuapp.com/weather/india")
-
 p.then((response) => {
-    () => {
-        return response.json()
-    }
-})
-.then((response) => {
-    console.log(response)
+    return response.json()
+}).then((response2) => {
+    console.log(response2)
 })
