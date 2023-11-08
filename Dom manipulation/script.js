7ad3b7a35e5dae11a2edba6ce0a874511ad7f75a
Aman Tomar <93078419+AmanTomar02@users.noreply.github.com>
2023-11-08 18:09:37 +0000

    cobfi

--- Dom manipulation/script.js
+++ Dom manipulation/script.js
@@ -1,16 +1,22 @@
-const mybody = document.body;
-console.log(mybody)
+// const mybody = document.body;
+// console.log(mybody)
 
-const box2 = document.getElementById("box2");
-console.log(box2)
+// const box2 = document.getElementById("box2");
+// console.log(box2)
 
-const divs = document.getElementsByTagName('div')
-console.log(divs)
+// const divs = document.getElementsByTagName('div')
+// console.log(divs)
 
-const divs1 = document.getElementsByTagName('p')
-console.log(divs1)
+// const divs1 = document.getElementsByTagName('p')
+// console.log(divs1)
 
 
-const boxes = document.getElementsByClassName('random')
-console.log(boxes)
+// const boxes = document.getElementsByClassName('random')
+// console.log(boxes)
 
+
+
+/**how to modify html */
+
+const box1= document.getElementById("box1")
+box1.innerHTML= "<h3>hey</h3>"
