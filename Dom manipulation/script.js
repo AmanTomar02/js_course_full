c49d4a015153c93952d73222d1321861a41ce782
Aman Tomar <93078419+AmanTomar02@users.noreply.github.com>
2023-11-08 14:57:51 +0000

    cons

--- Dom manipulation/script.js
+++ Dom manipulation/script.js
@@ -6,3 +6,11 @@ console.log(box2)
 
 const divs = document.getElementsByTagName('div')
 console.log(divs)
+
+const divs1 = document.getElementsByTagName('p')
+console.log(divs1)
+
+
+const boxes = document.getElementsByClassName('random')
+console.log(boxes)
+
